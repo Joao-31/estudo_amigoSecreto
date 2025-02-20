143f95edaebf5ef86c7c8bd1cc635a119726413b
João Paulo <joaopauloc.0731@gmail.com>
2025-02-20 18:06:26 -0300

    Implementa uma função para atualizar a lista de amigos

--- app.js
+++ app.js
@@ -1,12 +1,45 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
-let nomesAmigos = [];
+let amigos = [];
 
-function adicionarAmigo(nome){
-    nomesAmigos = nome;
-    
+function adicionarAmigo(){
+    let nome = document.querySelector('input').value;
+
+    if(nome==''){
+        alert('Por favor, insira um nome.');
+    }
+    else{amigos.push(nome);
+
+    atualizarLista();
+    }
+
+    limparCampo();
 }
 
-adicionarAmigo('João');
-adicionarAmigo('Pedro');
-console.log(nomesAmigos);
+function limparCampo(){
+    nome = document.querySelector('input');
+    nome.value = '';
+}
+
+function atualizarLista(){
+    let listaAmigos = document.getElementById('listaAmigos')
+
+    listaAmigos.innerHTML = '';
+
+    for( let i = 0; i<amigos.length; i++){
+
+        let itemLista = document.createElement('li');
+
+        itemLista.textContent = amigos[i];
+
+        listaAmigos.appendChild(itemLista);
+
+        console.log(amigos[i]);
+
+    }
+
+
+}
+
+adicionarAmigo();
+
